e0de937b3313211e93e2b4ebcf613430c7cd6aaf
jean <jeansanto195@gmail.com>
2024-07-25 14:56:27 -0300

    Projeto facul Angula

---
+++ shoppelotas/.angular/cache/17.0.7/vite/deps/primeng_button.js
@@ -0,0 +1,16 @@
+import {
+  Button,
+  ButtonDirective,
+  ButtonModule
+} from "./chunk-3X3MOVUA.js";
+import "./chunk-BLJLLVKD.js";
+import "./chunk-BVESBPHJ.js";
+import "./chunk-5N4UIHZB.js";
+import "./chunk-WTKTNTGT.js";
+import "./chunk-IH2XHIWS.js";
+export {
+  Button,
+  ButtonDirective,
+  ButtonModule
+};
+//# sourceMappingURL=primeng_button.js.map
